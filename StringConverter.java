82449e9f030608f84e0dcc0f0095785ee1072a55
cakai20-jpeg <cakai20@punahou.edu>
2020-04-20 08:37:31 +0000

    finished reverse and palindrome i hope, getting an error that i cant start reverse with public?

---
+++ StringConverter.java
@@ -0,0 +1,45 @@
+public class StringConverter
+{
+    public static void main (String[]args) 
+    {
+        
+        public static String reverse(String str)
+        {
+            
+            for(int i = str.length() - 1; i>= 0; i--)
+            {
+                return str.charAt(i);
+            }
+        }
+        public static boolean checkPalindrome(String str)
+        {
+            reverseStr = "";
+            
+            for(int i = str.length() - 1; i>= 0; i--)
+            {
+                reverseS = tr.charAt(i);
+            }
+            
+            str.toLowerCase();
+            str.replace(" ", "");
+            
+            reverseStr.toLowerCase();
+            reverseStr.replace(" ","");
+            
+            if (str == reverseStr)
+            {
+                return str + "is a palindrome";
+            }
+        }
+        
+        public static String pigLatinate(String str)
+        {
+            
+        }
+        
+        public static String shorthand(String str)
+        {
+            
+        }
+    }
+}
